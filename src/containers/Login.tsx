d0ad64b2bed5c4b874c1d873ed2437de7b2afb2f
byungmin12 <kbm940526@naver.com>
2023-01-12 11:11:39 +0900

    #4 data-fetching
    userId를 입력받고 github에 이 아이디가 존재하는 지를 확인 후
    issuer 페이지로 이동하게끔 로직 변경

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Input, InputLabel, Paper as P, styled, Typography } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import octokit from '../apis/octokit'
+import useRepositories from '../stores/useUserId'
 
 const Wrapper = styled(Box)`
   width: 100vw;
@@ -22,9 +25,29 @@ const Paper = styled(P)`
 
 function Login() {
   const [idTextValue, setIdTextValue] = React.useState('')
+  const handlerStoreUserId = useRepositories(state=>state.handlerStoreUserId)
+  const navigate = useNavigate()
 
   const handlerAddGithubID = () => {
-    localStorage.setItem('githubId', idTextValue)
+    (
+      async () => {
+        try {
+          const getUser = await octokit.request('GET /users/{username}', {
+            username: idTextValue
+          })
+
+          if(getUser.status ===200){
+            handlerStoreUserId(idTextValue)
+            navigate("/")
+          }
+        }catch (e){
+          alert("아이디가 없습니다.\n 다시 확인해주세요")
+        }
+        setIdTextValue("")
+      }
+    )().catch((e)=>{
+      alert(e)
+    })
   }
 
   const handlerChangeIdTextValue = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
